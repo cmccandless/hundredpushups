052c154677a4742ce7c41dae3f68848ff8438d80
Corey McCandless <corey.mccandless@atlasied.com>
2018-07-06 12:49:09 -0400

    initial commit

---
+++ generate.py
@@ -0,0 +1,240 @@
+import requests
+import requests_cache
+from lxml import html
+import re
+from json import dumps as json_dumps
+import sys
+import markdown_generator as mdg
+
+requests_cache.install_cache()
+
+TIME_PER_PUSHUP = 2  # seconds
+
+BASE_URL = 'https://hundredpushups.com/'
+
+RGX_NUMBER = re.compile('^(\d+)\+?$')
+RGX_REST = re.compile(
+    'rest (\d+) seconds between each set',
+    flags=re.IGNORECASE
+)
+RGX_RULE = re.compile(
+    '(\d+)-(\d+)|< (\d+)'
+)
+
+
+class PushupSetGroup(object):
+    def __init__(self, rule, rest=0, sets=None):
+        self.rule = rule
+        self.rest = rest
+        self.sets = sets or []
+
+    def __in__(self, value):
+        return value >= self.rule[0] and value <= self.rule[1]
+
+    def __lt__(self, other):
+        d = self.rule[0] - other.rule[0]
+        if d != 0:
+            return d < 0
+        return self.rule[1] < other.rule[1]
+
+    def estimate(self):
+        return (
+            sum(TIME_PER_PUSHUP * p for p in self.sets) +
+            self.rest * (len(self.sets) - 1)
+        )
+
+    def __str__(self):
+        return '{1}-{2} pushups: {0}+'.format(
+            ', '.join(map(str, self.sets)),
+            *self.rule
+        )
+
+    def json(self):
+        return {
+            'min': self.rule[0],
+            'max': self.rule[1],
+            'sets': self.sets
+        }
+
+
+class Day(object):
+    def __init__(self, number=0, rest=0):
+        self.number = number
+        self.set_groups = []
+
+    def add_set_group(self, grp):
+        self.set_groups.append(grp)
+        self.set_groups.sort()
+
+    def find_set_group(self, value):
+        for grp in self.set_groups:
+            if value in grp:
+                return grp
+
+    def estimate(self):
+        return sum(
+            grp.estimate()
+            for grp in self.set_groups
+        ) // len(self.set_groups) // 60
+
+    def __str__(self):
+        return '\n'.join([
+            (
+                f'Day {self.number} (rest {self.rest}s; '
+                f'about {self.estimate()}min)'
+            ),
+            *map(str, self.set_groups)
+        ])
+
+    def json(self):
+        return {
+            'day_number': self.number,
+            'rest': self.rest,
+            'set_groups': [g.json() for g in self.set_groups]
+        }
+
+
+class Week(object):
+    def __init__(self, number, days=None):
+        self.number = number
+        self.days = days or []
+
+    def __str__(self):
+        return '\n'.join([
+            f'Week {self.number}',
+            *map(str, self.days)
+        ])
+
+    def json(self):
+        return {
+            'week_number': self.number,
+            'days': [d.json() for d in self.days]
+        }
+
+
+def get_week(n):
+    url = '{}week{}.html'.format(BASE_URL, n)
+    resp = requests.get(url)
+    tree = html.fromstring(resp.content)
+
+    week = Week(n)
+    for table in tree.xpath('//table'):
+        day = Day()
+        for i, c in enumerate(table.xpath('thead/tr/th/text()')):
+            if c.startswith('DAY'):
+                day.number = int(c.split(' ')[1])
+                continue
+            m = RGX_REST.match(c)
+            if m:
+                day.rest = int(m.group(1))
+                continue
+        current_set = []
+        for i, c in enumerate(table.xpath('tbody/tr/td/text()')):
+            m = RGX_RULE.match(c)
+            if m:
+                if m.group(3):
+                    rule = (0, int(m.group(3)))
+                else:
+                    rule = (
+                        int(m.group(1)),
+                        int(m.group(2))
+                    )
+                day.add_set_group(PushupSetGroup(rule, day.rest))
+                continue
+            if c.startswith('SET'):
+                if current_set:
+                    for count, s in zip(current_set, day.set_groups):
+                        s.sets.append(count)
+                current_set.clear()
+                continue
+            m = RGX_NUMBER.match(c)
+            if m:
+                current_set.append(int(m.group(1)))
+                continue
+        if current_set:
+            for count, s in zip(current_set, day.set_groups):
+                s.sets.append(count)
+        week.days.append(day)
+    return week
+
+
+def create_json(weeks, filename):
+    with open(filename, 'w') as f:
+        f.write(json_dumps([w.json() for w in weeks], indent=2))
+
+
+def create_md(weeks, filename):
+    with open(filename, 'w') as f:
+        writer = mdg.Writer(f)
+        writer.write_heading('100 Pushups')
+        writer.write('from ')
+        writer.writeline(mdg.link(
+            'https://hundredpushups.com',
+            'hundredpushups.com'
+        ))
+        writer.writeline()
+
+        writer.write_heading('Weeks', 2)
+        writer.writeline()
+        # unordered = mdg.List()
+        # for i in range(1, 7):
+        #     unordered.append(mdg.link(
+        #         f'#week{i}',
+        #         f'Week {i}'
+        #     ))
+        # writer.write(unordered)
+        for i in range(1, 7):
+            writer.write('- ')
+            writer.writeline(mdg.link(
+                f'#week{i}',
+                f'Week {i}'
+            ))
+            for j in range(1, 4):
+                writer.write('  - ')
+                writer.writeline(mdg.link(
+                    f'#week{i}day{j}',
+                    f'Day {j}'
+                ))
+        writer.writeline()
+
+        for i, week in enumerate(weeks, 1):
+            writer.write_heading(f'Week {i}', 2)
+            writer.writeline()
+            for j, day in enumerate(week.days, 1):
+                writer.write_heading(
+                    f'Week {i}: Day {j} (rest: {day.rest}s)',
+                    3
+                )
+                writer.writeline(f'Total: ~{day.estimate()}min')
+                writer.writeline()
+                table = mdg.Table()
+                table.add_column('Pushups')
+                for grp in day.set_groups:
+                    table.add_column('{}-{}'.format(*grp.rule))
+                rows = list(zip(*(grp.sets for grp in day.set_groups)))
+                for k, row in enumerate(rows, 1):
+                    if k < len(rows):
+                        table.append(f'Set {k}', *row)
+                    else:
+                        table.append(
+                            f'Set {k}',
+                            *(f'{n}+' for n in row)
+                        )
+                writer.write(table)
+            if i in (2, 4):
+                writer.writeline(mdg.strong(
+                    'Do exhaustion test at the end of this week'
+                ))
+
+
+if __name__ == '__main__':
+    weeks = [get_week(i) for i in range(1, 7)]
+    mode = '--print' if len(sys.argv) < 2 else sys.argv[1]
+    filename = None if len(sys.argv) < 3 else sys.argv[2]
+    if mode == '--print':
+        for week in weeks:
+            print(week)
+    elif mode == '--json':
+        create_json(weeks, filename or 'pushups.json')
+    elif mode == '--md':
+        create_md(weeks, filename or 'pushups.md')
---
+++ index.md
@@ -0,0 +1,256 @@
+# 100 Pushups
+from [hundredpushups.com](https://hundredpushups.com)
+
+## Weeks
+
+- [Week 1](#week1)
+  - [Day 1](#week1day1)
+  - [Day 2](#week1day2)
+  - [Day 3](#week1day3)
+- [Week 2](#week2)
+  - [Day 1](#week2day1)
+  - [Day 2](#week2day2)
+  - [Day 3](#week2day3)
+- [Week 3](#week3)
+  - [Day 1](#week3day1)
+  - [Day 2](#week3day2)
+  - [Day 3](#week3day3)
+- [Week 4](#week4)
+  - [Day 1](#week4day1)
+  - [Day 2](#week4day2)
+  - [Day 3](#week4day3)
+- [Week 5](#week5)
+  - [Day 1](#week5day1)
+  - [Day 2](#week5day2)
+  - [Day 3](#week5day3)
+- [Week 6](#week6)
+  - [Day 1](#week6day1)
+  - [Day 2](#week6day2)
+  - [Day 3](#week6day3)
+
+## Week 1
+
+### Week 1: Day 1 (rest: 60s)
+Total: ~4min
+
+| Pushups | 0-5 | 6-10 | 11-20 |
+|:--- |:--- |:--- |:--- |
+| Set 1 | 2 | 6 | 10 |
+| Set 2 | 3 | 6 | 12 |
+| Set 3 | 2 | 4 | 7 |
+| Set 4 | 2 | 4 | 7 |
+| Set 5 | 3+ | 5+ | 9+ |
+
+### Week 1: Day 2 (rest: 60s)
+Total: ~5min
+
+| Pushups | 0-5 | 6-10 | 11-20 |
+|:--- |:--- |:--- |:--- |
+| Set 1 | 3 | 6 | 10 |
+| Set 2 | 4 | 8 | 12 |
+| Set 3 | 2 | 6 | 8 |
+| Set 4 | 3 | 6 | 8 |
+| Set 5 | 4+ | 7+ | 12+ |
+
+### Week 1: Day 3 (rest: 60s)
+Total: ~5min
+
+| Pushups | 0-5 | 6-10 | 11-20 |
+|:--- |:--- |:--- |:--- |
+| Set 1 | 4 | 8 | 11 |
+| Set 2 | 5 | 10 | 15 |
+| Set 3 | 4 | 7 | 9 |
+| Set 4 | 4 | 7 | 9 |
+| Set 5 | 5+ | 10+ | 13+ |
+
+## Week 2
+
+### Week 2: Day 1 (rest: 60s)
+Total: ~5min
+
+| Pushups | 0-5 | 6-10 | 11-20 |
+|:--- |:--- |:--- |:--- |
+| Set 1 | 4 | 9 | 14 |
+| Set 2 | 6 | 11 | 14 |
+| Set 3 | 4 | 8 | 10 |
+| Set 4 | 4 | 8 | 10 |
+| Set 5 | 6+ | 11+ | 15+ |
+
+### Week 2: Day 2 (rest: 90s)
+Total: ~7min
+
+| Pushups | 0-5 | 6-10 | 11-20 |
+|:--- |:--- |:--- |:--- |
+| Set 1 | 5 | 10 | 14 |
+| Set 2 | 6 | 12 | 16 |
+| Set 3 | 4 | 9 | 12 |
+| Set 4 | 4 | 9 | 12 |
+| Set 5 | 7+ | 13+ | 17+ |
+
+### Week 2: Day 3 (rest: 120s)
+Total: ~9min
+
+| Pushups | 0-5 | 6-10 | 11-20 |
+|:--- |:--- |:--- |:--- |
+| Set 1 | 5 | 12 | 16 |
+| Set 2 | 7 | 13 | 17 |
+| Set 3 | 5 | 10 | 14 |
+| Set 4 | 5 | 10 | 14 |
+| Set 5 | 8+ | 15+ | 20+ |
+
+**Do exhaustion test at the end of this week**
+## Week 3
+
+### Week 3: Day 1 (rest: 60s)
+Total: ~5min
+
+| Pushups | 16-20 | 21-25 |
+|:--- |:--- |:--- |
+| Set 1 | 10 | 12 |
+| Set 2 | 12 | 17 |
+| Set 3 | 7 | 13 |
+| Set 4 | 7 | 13 |
+| Set 5 | 9+ | 17+ |
+
+### Week 3: Day 2 (rest: 90s)
+Total: ~8min
+
+| Pushups | 16-20 | 21-25 |
+|:--- |:--- |:--- |
+| Set 1 | 10 | 14 |
+| Set 2 | 12 | 19 |
+| Set 3 | 8 | 14 |
+| Set 4 | 8 | 14 |
+| Set 5 | 12+ | 19+ |
+
+### Week 3: Day 3 (rest: 120s)
+Total: ~10min
+
+| Pushups | 16-20 | 21-25 |
+|:--- |:--- |:--- |
+| Set 1 | 11 | 16 |
+| Set 2 | 13 | 21 |
+| Set 3 | 9 | 15 |
+| Set 4 | 9 | 15 |
+| Set 5 | 13+ | 21+ |
+
+## Week 4
+
+### Week 4: Day 1 (rest: 60s)
+Total: ~6min
+
+| Pushups | 16-20 | 21-25 |
+|:--- |:--- |:--- |
+| Set 1 | 12 | 18 |
+| Set 2 | 14 | 22 |
+| Set 3 | 11 | 16 |
+| Set 4 | 10 | 16 |
+| Set 5 | 16+ | 25+ |
+
+### Week 4: Day 2 (rest: 90s)
+Total: ~9min
+
+| Pushups | 16-20 | 21-25 |
+|:--- |:--- |:--- |
+| Set 1 | 14 | 20 |
+| Set 2 | 16 | 25 |
+| Set 3 | 12 | 20 |
+| Set 4 | 12 | 20 |
+| Set 5 | 18+ | 28+ |
+
+### Week 4: Day 3 (rest: 120s)
+Total: ~11min
+
+| Pushups | 16-20 | 21-25 |
+|:--- |:--- |:--- |
+| Set 1 | 16 | 23 |
+| Set 2 | 18 | 28 |
+| Set 3 | 13 | 23 |
+| Set 4 | 13 | 23 |
+| Set 5 | 20+ | 33+ |
+
+**Do exhaustion test at the end of this week**
+## Week 5
+
+### Week 5: Day 1 (rest: 60s)
+Total: ~7min
+
+| Pushups | 31-35 | 36-40 |
+|:--- |:--- |:--- |
+| Set 1 | 17 | 28 |
+| Set 2 | 19 | 35 |
+| Set 3 | 15 | 25 |
+| Set 4 | 15 | 22 |
+| Set 5 | 20+ | 35+ |
+
+### Week 5: Day 2 (rest: 45s)
+Total: ~9min
+
+| Pushups | 31-35 | 36-40 |
+|:--- |:--- |:--- |
+| Set 1 | 10 | 18 |
+| Set 2 | 10 | 18 |
+| Set 3 | 13 | 20 |
+| Set 4 | 13 | 20 |
+| Set 5 | 10 | 14 |
+| Set 6 | 10 | 14 |
+| Set 7 | 9 | 16 |
+| Set 8 | 25+ | 40+ |
+
+### Week 5: Day 3 (rest: 45s)
+Total: ~10min
+
+| Pushups | 31-35 | 36-40 |
+|:--- |:--- |:--- |
+| Set 1 | 13 | 18 |
+| Set 2 | 13 | 18 |
+| Set 3 | 15 | 20 |
+| Set 4 | 15 | 20 |
+| Set 5 | 12 | 17 |
+| Set 6 | 12 | 17 |
+| Set 7 | 10 | 20 |
+| Set 8 | 30+ | 45+ |
+
+## Week 6
+
+### Week 6: Day 1 (rest: 60s)
+Total: ~9min
+
+| Pushups | 46-50 | 51-60 |
+|:--- |:--- |:--- |
+| Set 1 | 25 | 40 |
+| Set 2 | 30 | 50 |
+| Set 3 | 20 | 25 |
+| Set 4 | 15 | 25 |
+| Set 5 | 40+ | 50+ |
+
+### Week 6: Day 2 (rest: 45s)
+Total: ~12min
+
+| Pushups | 46-50 | 51-60 |
+|:--- |:--- |:--- |
+| Set 1 | 14 | 20 |
+| Set 2 | 14 | 20 |
+| Set 3 | 15 | 23 |
+| Set 4 | 15 | 23 |
+| Set 5 | 14 | 20 |
+| Set 6 | 14 | 20 |
+| Set 7 | 10 | 18 |
+| Set 8 | 10 | 18 |
+| Set 9 | 44+ | 53+ |
+
+### Week 6: Day 3 (rest: 45s)
+Total: ~12min
+
+| Pushups | 46-50 | 51-60 |
+|:--- |:--- |:--- |
+| Set 1 | 13 | 22 |
+| Set 2 | 13 | 22 |
+| Set 3 | 17 | 30 |
+| Set 4 | 17 | 30 |
+| Set 5 | 16 | 25 |
+| Set 6 | 16 | 25 |
+| Set 7 | 14 | 18 |
+| Set 8 | 14 | 18 |
+| Set 9 | 50+ | 55+ |
+
